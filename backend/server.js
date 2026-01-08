b7fb6d21a200e6c73fddb7d3bac651010d460edb
philipfleischer <fleischer1@live.no>
2026-01-08 18:34:12 +0100

    Made the HTTP functionality: GET, POST, PUT and DELETE, but they are not particularly following the best practices, which I will improve later

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,25 @@ import express from 'express';
 
 const app = express();
 
+// What is an Endpoint?
+// An endpoint is a combination of an URL + HTTP method that lets the client interact with a specific resource
+
+app.get('/api/notes', (req, res) => {
+  res.status(200).send('You have got 10 notes');
+});
+
+app.post('/api/notes', (req, res) => {
+  res.status(201).json({ message: 'Note created successfully!' });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  res.status(200).json({ message: 'Note updated successfully!' });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  res.status(200).json({ message: 'Note deleted successfully!' });
+});
+
 app.listen(5001, () => {
   console.log('Server started on PORT: 5001');
 });
