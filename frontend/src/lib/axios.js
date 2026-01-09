bf01720c64e636c6373a1555df300f91dab63d51
philipfleischer <fleischer1@live.no>
2026-01-09 16:46:13 +0100

    Typo in acios.js fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // In production, there is no localhost, THEREFORE I need to make it dynamic
 // The one using this, will import the current URL and use that!
-const BASE_URL = import.meta.env.MODE === "development"! 'http://localhost:5001/api' : "/api";
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
