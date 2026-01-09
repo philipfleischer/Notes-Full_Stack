f40d9861dbfcced501c8661a3a48c46b3004620b
philipfleischer <fleischer1@live.no>
2026-01-09 09:15:41 +0100

    Done: - Imported the Note to the notesController.js file and tested the async function getAllNotes. - Downloaded the Postman application and checked for the results of the HTTP GET request using this link: http://localhost:5001/api/notes and got out {}, which was expected. - All the controllers work and added a new controller to get a specific Note. - Edited getAllNotes to be shown in desc order, so that newly created notes are displayed first. - Controllers I have now are: getAllNotes(), getNoteByID(), createNote(), updateNote() and deleteNote().

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { createNote, deleteNote, getAllNotes, updateNote } from '../controllers/notesController.js';
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  getNoteById,
+  updateNote,
+} from '../controllers/notesController.js';
 
 const router = express.Router();
 
 router.get('/', getAllNotes);
+router.get('/:id', getNoteById);
 router.post('/', createNote);
 router.put('/:id', updateNote);
-router.put('/:id', deleteNote);
+router.delete('/:id', deleteNote);
 
 export default router;
